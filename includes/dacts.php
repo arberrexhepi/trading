e82560835f05e1cec5c1541345daf0f3674fc9ee
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2025-01-17 15:37:08 -0400

    Add includes/dacts.php

--- includes/dacts.php
+++ includes/dacts.php
@@ -1,3 +1,23 @@
 <?php
 // DACTS framework logic
+
+function evaluateConfluence($data) {
+    $score = 0;
+
+    // Example logic to calculate a confluence score
+     if ($data['trend'] === 'bullish') {
+        $score += 30;
+    }
+
+    if ($data['sentiment'] === 'positive') {
+        $score += 20;
+    }
+
+    if ($data['volume'] > 1000000) {
+        $score += 50;
+    }
+
+    return $score;
+}
+
 ?>
