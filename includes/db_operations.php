e59e38b5f61c5dd9952df43fe5d4432afb85b8a3
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2025-01-17 19:01:59 -0400

    Add database operations for user preferences

---
+++ includes/db_operations.php
@@ -0,0 +1,7 @@
+<?php
+
+function getUserPreferences($userId) {
+    global $db;
+    $query = "SELECT * FROM preferences WHERE user_id = ?";
+    $stmt = $db-vre"<7</stmmarepare=contol.\n    $stmm-operation.(operational-get)@0\.\n
+substmt
